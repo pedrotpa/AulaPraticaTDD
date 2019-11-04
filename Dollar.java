fc857a063c7ce2a6981bc45a2fb27f934edbae1f
Pedro Thomas <pedro.thomas94@gmail.com>
2019-11-04 01:06:09 -0300

    COMMIT & PUSH #2

--- Dollar.java
+++ Dollar.java
@@ -1,5 +1,14 @@
 class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
- }
+	int amount;
+	Dollar(int amount) {
+		this.amount= amount;
+	}
+	void times(int multiplier) {
+      amount= amount * multiplier;
+   }
+
+	public boolean equals(Object object)  {
+   Dollar dollar = (Dollar) object;
+   return amount == dollar.amount;
+	}
+}
